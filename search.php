94b3b47c29426c32a918cffc93c8e1200f5d6310
kotarou99 <125839742+kotarou99@users.noreply.github.com>
2023-05-11 11:09:46 +0900

    Add files via upload

---
+++ search.php
@@ -0,0 +1,73 @@
+<?php
+require_once "define02.php";
+
+$area_id = filter_input(INPUT_GET, "area_id");
+$ani_type = filter_input(INPUT_GET, "ani_type");
+$ani_name = filter_input(INPUT_GET, "ani_name");
+$status_id = filter_input(INPUT_GET, "status_id");
+
+
+// ANDをどうするか
+
+try {
+    // DBへ接続
+    $driver = DB_DRIVER;
+    $host = DB_HOST;
+    $dbName = DB_NAME;
+    $charset = DB_CHRSET;
+  
+    $db = new PDO("{$driver}:host={$host};dbname={$dbName};charaset={$charset}", DB_USER,DB_PASS);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    // whereの作成
+    $where = "";
+    if($area_id) {
+    $where .= " AREA_ID = :area_id ";
+    }
+
+    if($ani_type) {
+        if( !empty($where)) {
+            $where .= " AND "; 
+        }
+    $where .= " ANI_TYPE = :ani_type ";
+    }
+
+    if($ani_name) {
+        if( !empty($where)) {
+            $where .= " AND "; 
+        }
+    $where .= " ANI_TYPE = :ani_type ";
+    }
+
+    if($ani_type) {
+        if( !empty($where)) {
+            $where .= " AND "; 
+        }
+    $where .= " ANI_TYPE = :ani_type ";
+    }
+
+
+    if( !empty($where) ) {
+    $where = " WHERE ". "$where";
+    }
+  
+    // DBへSQLの実行
+    $sql = "SELECT * FROM ANIMALS {$where}";
+    $stmt = $db->prepare($sql);
+
+    $stmt->execute();
+
+    // 3. SQLの結果を処理
+    $rows = [];
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $rows[] = $row;
+    }
+
+
+  }catch(PDOException $e) {
+      print $e->getMessage();
+  }
+  
+
+
+?>
